2f1d388db11126ffe8780d7846d09468d5a25d0e
Luke Chambers <lukechambers91@gmail.com>
2017-06-10 10:41:46 -0400

    added firebase initializer

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,15 @@
+
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyAQrmHX8bpyL39ccDq8P0rQmc85Y2Ziq88",
+    authDomain: "employeeclassproject.firebaseapp.com",
+    databaseURL: "https://employeeclassproject.firebaseio.com",
+    projectId: "employeeclassproject",
+    storageBucket: "employeeclassproject.appspot.com",
+    messagingSenderId: "258953559325"
+  };
+  firebase.initializeApp(config);
+
 $(document).ready(function() {
 	$('#sections').on('click', 'button.submitButton', function() {
 		console.log("Test");
